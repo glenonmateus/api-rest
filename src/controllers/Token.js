37e74fe00eadf5266c9f29913e47f556a141d38e
Glenon Mateus <glenonmateus+github@gmail.com>
2026-06-24 15:52:54 -0300

    refact: use arrow functions

--- src/controllers/Token.js
+++ src/controllers/Token.js
@@ -2,28 +2,35 @@ import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
 class TokenController {
-  async store(req, res) {
+  store = async (req, res) => {
     try {
       const { email, password } = req.body;
-      if (!email || !password)
-        return res
-          .status(400)
-          .json({ errors: ["Email and password are required"] });
-      const user = await User.findOne({ where: { email } });
-      if (!user || !(await user.isValidPassword(password))) {
+      if (!(await this.isValidCredentials(email, password))) {
         return res.status(401).json({ errors: ["Invalid credentials"] });
       }
-      const { id } = user;
-      const token = jwt.sign({ id, email }, process.env.JWT_TOKEN_SECRET, {
-        expiresIn: process.env.JWT_TOKEN_EXPIRATION,
-      });
-      return res.json({ access_token: token });
+      return res.json({ access_token: this.generateToken(email) });
     } catch (error) {
+      console.error(error);
       return res
         .status(400)
         .json({ errors: error.errors.map((e) => e.message) });
     }
-  }
+  };
+
+  isValidCredentials = async (email, password) => {
+    try {
+      const user = await User.findOne({ where: { email } });
+      return user || (await user.isValidPassword(password));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  generateToken = (email) => {
+    return jwt.sign({ email }, process.env.JWT_TOKEN_SECRET, {
+      expiresIn: process.env.JWT_TOKEN_EXPIRATION,
+    });
+  };
 }
 
 export default new TokenController();
