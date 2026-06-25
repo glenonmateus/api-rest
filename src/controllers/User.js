63d4169df5fc8df619b58b86968291e78dcf3615
Glenon Mateus <glenonmateus+github@gmail.com>
2026-06-25 13:43:44 -0300

    refact: add attributes to return users

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -3,10 +3,25 @@ import User from "../models/User.js";
 class UserController {
   index = async (req, res) => {
     try {
-      return res.json(await User.findAll());
+      return res.json(
+        await User.findAll({ attributes: ["id", "name", "email"] }),
+      );
     } catch (error) {
       console.error(error);
       return res.status(400).json(null);
+    }
+  };
+
+  show = async (req, res) => {
+    try {
+      const user = await User.findByPk(req.params.id);
+      const { id, name, email } = user;
+      return res.json({ id, name, email });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((e) => e.message) });
     }
   };
 
@@ -21,35 +36,10 @@ class UserController {
     }
   };
 
-  show = async (req, res) => {
-    try {
-      return res.json(await User.findByPk(req.params.id));
-    } catch (error) {
-      console.error(error);
-      return res
-        .status(400)
-        .json({ errors: error.errors.map((e) => e.message) });
-    }
-  };
-
-  delete = async (req, res) => {
-    try {
-      const user = await User.destroy({
-        where: { id: req.params.id, email: req.userEmail },
-      });
-      if (!user)
-        return res.status(401).json({ errors: ["Deleted not allowed"] });
-      return res.json(null);
-    } catch (error) {
-      console.error(error);
-      return res.status(400).json(null);
-    }
-  };
-
   update = async (req, res) => {
     try {
       const user = await User.update(req.body, {
-        where: { id: req.params.id, email: req.userEmail },
+        where: { email: req.userEmail },
       });
       if (!user[0]) {
         return res.status(401).json({ errors: ["Updated not allowed"] });
@@ -58,6 +48,20 @@ class UserController {
     } catch (error) {
       console.error(error);
       return res.status(500).json(null);
+    }
+  };
+
+  delete = async (req, res) => {
+    try {
+      const user = await User.destroy({
+        where: { email: req.userEmail },
+      });
+      if (!user)
+        return res.status(401).json({ errors: ["Deleted not allowed"] });
+      return res.json(null);
+    } catch (error) {
+      console.error(error);
+      return res.status(400).json(null);
     }
   };
 }
