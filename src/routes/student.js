04874c7cbf5d4a59180f43ea14ee7194d42ee4b1
Glenon Mateus <glenonmateus+github@gmail.com>
2026-06-25 15:04:09 -0300

    feat: enable authentication student store, update and delete

--- src/routes/student.js
+++ src/routes/student.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import StudentController from "../controllers/Student.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = new Router();
 
 router.get("/students", StudentController.index);
-router.post("/students/store", StudentController.store);
 router.get("/students/:id", StudentController.show);
-router.put("/students/:id", StudentController.update);
-router.delete("/students/:id", StudentController.delete);
+router.post("/students/store", isAuthenticated, StudentController.store);
+router.put("/students/:id", isAuthenticated, StudentController.update);
+router.delete("/students/:id", isAuthenticated, StudentController.delete);
 
 export { router };
