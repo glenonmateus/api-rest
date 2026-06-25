3af68f1959342f005cb5a07af8599e95e44f8ddc
Glenon Mateus <glenonmateus+github@gmail.com>
2026-06-24 22:06:40 -0300

    feat(user): enabled isAuthenticated (index and update)

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import UserController from "../controllers/User.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = new Router();
 
-router.get("/users", UserController.index);
+router.get("/users", isAuthenticated, UserController.index);
 router.post("/users/store", UserController.store);
 router.get("/users/:id", UserController.show);
 router.delete("/users/:id", UserController.delete);
-router.put("/users/:id", UserController.update);
+router.put("/users/:id", isAuthenticated, UserController.update);
 
 export { router };
