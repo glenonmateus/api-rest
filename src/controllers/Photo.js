d3ffab68ec68037d1ecdefdd07976c3dba1a34a3
Glenon Mateus <glenonmateus+github@gmail.com>
2026-06-25 18:58:10 -0300

    feat: create photo on database

--- src/controllers/Photo.js
+++ src/controllers/Photo.js
@@ -1,13 +1,26 @@
 import multer from "multer";
 import multerConfig from "../config/multer.js";
+import Photo from "../models/Photo.js";
 
 const photo = multer(multerConfig).single("photo");
 
 class PhotoController {
   store = (req, res) => {
-    return photo(req, res, (error) => {
-      if (error) return res.status(400).json({ errors: [error.code] });
-      return res.json(req.file);
+    return photo(req, res, async (error) => {
+      try {
+        if (error) return res.status(400).json({ errors: [error.code] });
+        const { student_id } = req.body;
+        const { originalname, filename } = req.file;
+        const photo = await Photo.create({
+          originalname,
+          filename,
+          student_id,
+        });
+        return res.json(photo);
+      } catch (error) {
+        console.error(error);
+        return res.status(400).json({ errors: ["Invalid student_id"] });
+      }
     });
   };
 }
