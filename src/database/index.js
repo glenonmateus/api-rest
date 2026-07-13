aecd34087910f2d25ea31a42f1fc4e284a8c56ab
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-13 20:50:39 -0300

    feat: test database connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,17 @@
 import Sequelize from "sequelize";
-import databaseConfig from "../config/database.cjs";
+import databaseConfig from "../config/database.js";
 import Photo from "../models/Photo.js";
 import Student from "../models/Student.js";
 import User from "../models/User.js";
 
-const connection = new Sequelize(databaseConfig);
+const connection = new Sequelize(databaseConfig[process.env.NODE_ENV]);
+
+try {
+  await connection.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database: ", error);
+}
 
 const models = [Student, User, Photo];
 
